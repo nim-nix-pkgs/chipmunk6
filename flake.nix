ce52d8125fed2a03cf58713a99a15e8338780526
hugosenari <hugosenari@gmail.com>
2022-03-12 01:03:59 -0300

    chore: re index chipmunk6

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Bindings for Chipmunk2D 6.x physics library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."chipmunk6-master".url = "path:./master";
+  inputs."chipmunk6-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."chipmunk6-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Bindings for Chipmunk2D 6.x physics library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-chipmunk6-master.flake = false;
+  inputs.src-chipmunk6-master.owner = "fowlmouth";
+  inputs.src-chipmunk6-master.ref   = "refs/heads/master";
+  inputs.src-chipmunk6-master.repo  = "nimrod-chipmunk";
+  inputs.src-chipmunk6-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-chipmunk6-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-chipmunk6-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
